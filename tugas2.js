138a7c36cb513512a055c6bb372d1039b864e7d0
Yehezkiel Reynaldi Abraham <yehezkielreynaldi01@gmail.com>
2022-03-15 23:49:33 +0700

    Tugas 2 Javascript
    Tugas ini saya kirimkan untuk Team Niomic,Terimakasih.

---
+++ tugas2.js
@@ -0,0 +1,11 @@
+var angka = 27;
+var nama = "Yehezkiel Reynaldi Abraham";
+var benar = true;
+var kosong = null;
+var array = ['Yehezkiel Reynaldi Abraham',9,27,2001];
+
+console.log(angka);
+console.log(nama);
+console.log(benar);
+console.log(kosong);
+console.log(array);
